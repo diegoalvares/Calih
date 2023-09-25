ae0636b7300c4aef092b0a92c7506553b3c28c51
Diego Alvares de Macedo <101260437+diegoalvares@users.noreply.github.com>
2023-09-25 18:12:48 -0300

    atualizando.

--- saudacoes/ia_respostas.py
+++
@@ -1,5 +0,0 @@
-bd = ['bom dia chefe!', 'bom dia chefinho!', 'bom diaaaa', 'aeeee o chefinho chegou']
-bt = ['boa tarde!', 'boa tarde chefinho!', 'boa tardeeee', 'aeeee o chefinho chegou']
-bn = ['boa noite chefe!', 'boa noite chefinho!', 'boa noiteee', 'aeeee o chefinho chegou']
-oi = ['oi chefe!', 'oi chefinho!', 'oiiiii', 'olá chefe!', 'olá chefinho', 'aeeee o chefinho chegou']
-
